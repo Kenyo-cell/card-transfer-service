87df91fe3a42bf8dd68ee1ca8e070dd9eced83a9
Kenyo-cell <rekurt3000@gmail.com>
2022-03-12 00:09:35 +0300

    fix: updated test to right logic implementation

--- src/test/java/org/example/MoneyTransferControllerTest.java
+++ src/test/java/org/example/MoneyTransferControllerTest.java
@@ -1,7 +1,9 @@
 package org.example;
 
 import org.example.controller.MoneyTransferController;
+import org.example.exception.ConfirmationException;
 import org.example.exception.IncorrectInputException;
+import org.example.request.confirm.ConfirmData;
 import org.example.request.transfer.TransferData;
 import org.example.response.error.ErrorResponse;
 import org.example.response.success.SuccessResponse;
@@ -31,9 +33,10 @@ public class MoneyTransferControllerTest {
     @Test
     public void correctJsonDataProvidesOkCodeAndJsonMediaTypeAndCorrectContent() throws Exception {
         String testOperationId = "0";
-        String json = "{\"cardFromNumber\": \"0000 0000 0000 0000\",\"cardFromValidTill\":\"12/24\",\"cardFromCVV\":\"123\",\"cardToNumber\":\"1234 5678 9012 3456\",\"amount\":{\"value\":100,\"currency\":\"jopa\"}}";
+        String json = "{\"cardFromNumber\": \"0000000000000000\",\"cardFromValidTill\":\"12/24\",\"cardFromCVV\":\"123\",\"cardToNumber\":\"1234567890123456\",\"amount\":{\"value\":100,\"currency\":\"RUR\"}}";
         TransferData data = Jackson2ObjectMapperBuilder.json().build()
                 .readValue(json.getBytes(), TransferData.class);
+
         Mockito.when(service.transfer(data)).thenReturn(new SuccessResponse(testOperationId));
 
         var result = mockMvc.perform(post("/transfer")
@@ -49,11 +52,12 @@ public class MoneyTransferControllerTest {
     }
 
     @Test
-    public void shouldReturnBadGatewayRequestWithIncorrectData() throws Exception {
-        String message = "Incorrect data";
-        String json = "{\"cardFromNumber\": \"0000 0000 0000 0000\",\"cardFromValidTill\":\"12/24\",\"cardFromCVV\":\"123\",\"cardToNumber\":\"1234 5678 9012 3456\",\"amount\":{\"value\":100,\"currency\":\"jopa\"}}";
+    public void shouldReturnBadRequestWithIncorrectTransferData() throws Exception {
+        String message = "Card From Number and Card To Number should have different values";
+        String json = "{\"cardFromNumber\": \"0000000000000000\",\"cardFromValidTill\":\"12/24\",\"cardFromCVV\":\"123\",\"cardToNumber\":\"0000000000000000\",\"amount\":{\"value\":100,\"currency\":\"RUR\"}}";
         TransferData data = Jackson2ObjectMapperBuilder.json().build()
                         .readValue(json.getBytes(), TransferData.class);
+
         Mockito.when(service.transfer(data)).thenThrow(new IncorrectInputException(message));
 
         var result = mockMvc.perform(
@@ -71,12 +75,50 @@ public class MoneyTransferControllerTest {
     }
 
     @Test
-    public void correctConfirmOperationReturnsOkAndCorrectContent() {
+    public void correctConfirmOperationReturnsOkAndCorrectContent() throws Exception {
+        String operationId = "12345679";
+        String json = "{\"operationId\":\"12345678\",\"code\":\"0000\"}";
+        ConfirmData data = Jackson2ObjectMapperBuilder.json().build()
+                .readValue(json, ConfirmData.class);
 
+        Mockito.when(service.confirm(data)).thenReturn(new SuccessResponse(operationId));
+
+        var result = mockMvc.perform(
+                post("/confirmOperation")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(json)
+                )
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andReturn();
+
+        var response = Jackson2ObjectMapperBuilder.json().build()
+                        .readValue(result.getResponse().getContentAsByteArray(), SuccessResponse.class);
+
+        Assertions.assertEquals(operationId, response.operationId());
     }
 
     @Test
-    public void confirmShouldThrowException() {
+    public void shouldReturnBadRequestWithIncorrectConfirmData() throws Exception {
+        String exceptionMessage = "Invalid Confirmation Code";
+        String json = "{\"operationId\":\"12345678\",\"code\":\"0000\"}";
+        ConfirmData data = Jackson2ObjectMapperBuilder.json().build()
+                .readValue(json, ConfirmData.class);
 
+        Mockito.when(service.confirm(data)).thenThrow(new ConfirmationException(exceptionMessage));
+
+        var result = mockMvc.perform(
+                        post("/confirmOperation")
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .content(json)
+                )
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isBadGateway())
+                .andReturn();
+
+        var response = Jackson2ObjectMapperBuilder.json().build()
+                .readValue(result.getResponse().getContentAsByteArray(), ErrorResponse.class);
+
+        Assertions.assertEquals(exceptionMessage, response.message());
     }
 }

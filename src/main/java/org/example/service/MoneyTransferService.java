050b426917c8bcbf4758d1c237feaf0c4524e25c
Kenyo-cell <rekurt3000@gmail.com>
2022-03-13 01:25:18 +0300

    info: wrote basic test for service

--- src/main/java/org/example/service/MoneyTransferService.java
+++ src/main/java/org/example/service/MoneyTransferService.java
@@ -1,8 +1,6 @@
 package org.example.service;
 
-import org.example.exception.ConfirmationException;
 import org.example.exception.IncorrectInputException;
-import org.example.exception.TransferException;
 import org.example.repository.MoneyTransferRepository;
 import org.example.request.confirm.ConfirmData;
 import org.example.request.transfer.TransferData;
@@ -15,13 +13,12 @@ public class MoneyTransferService {
     @Autowired
     MoneyTransferRepository repository;
 
-    public SuccessResponse transfer(TransferData data) throws IncorrectInputException, TransferException {
-        repository.writeTransferTransaction();
-        return new SuccessResponse("213");
+    public SuccessResponse transfer(TransferData data) throws IncorrectInputException {
+        repository.writeTransferTransaction(data);
+        return new SuccessResponse("123");
     }
 
-    public SuccessResponse confirm(ConfirmData data) throws IncorrectInputException, ConfirmationException {
-        repository.writeTransferTransaction();
+    public SuccessResponse confirm(ConfirmData data) throws IncorrectInputException {
         return new SuccessResponse("213");
     }
 }
--- src/test/java/org/example/MoneyTransferServiceTest.java
+++ src/test/java/org/example/MoneyTransferServiceTest.java
@@ -1,11 +1,84 @@
 package org.example;
 
+import org.example.exception.IncorrectInputException;
+import org.example.repository.MoneyTransferRepository;
+import org.example.request.confirm.ConfirmData;
+import org.example.request.transfer.Amount;
+import org.example.request.transfer.TransferData;
+import org.example.response.success.SuccessResponse;
+import org.example.service.MoneyTransferService;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.Optional;
+
+@SpringBootTest
 public class MoneyTransferServiceTest {
-    @Test
-    public void shouldThrowExceptionDueToIncorrectData() {
+    @Autowired
+    private MoneyTransferService service;
 
+    @Test
+    public void shouldThrowExceptionDueToSameCardToAndFromNumbers() {
+        String operationId = "213";
+        TransferData data = new TransferData(
+                "0000000000000000",
+                "12/24",
+                "123",
+                "0000000000000000",
+                new Amount(10000, "RUR")
+        );
+        MoneyTransferRepository repository = Mockito.mock(MoneyTransferRepository.class);
+        Mockito.doNothing().when(repository).writeTransferTransaction(data);
+
+        Assertions.assertThrows(IncorrectInputException.class, () -> service.transfer(data));
     }
 
+    @Test
+    public void shouldThrowExceptionDueToCardDateExpired() {
+        String operationId = "213";
+        TransferData data = new TransferData(
+                "0000000000000000",
+                "12/20",
+                "123",
+                "1234567890123456",
+                new Amount(10000, "RUR")
+        );
+        MoneyTransferRepository repository = Mockito.mock(MoneyTransferRepository.class);
+        Mockito.doNothing().when(repository).writeTransferTransaction(data);
+
+        Assertions.assertThrows(IncorrectInputException.class, () -> service.transfer(data));
+    }
+
+    @Test
+    public void transferShouldReturnSuccessResponse() {
+        String operationId = "213";
+        TransferData data = new TransferData(
+                "0000000000000000",
+                "12/24",
+                "123",
+                "1234567890123456",
+                new Amount(10000, "RUR")
+        );
+        MoneyTransferRepository repository = Mockito.mock(MoneyTransferRepository.class);
+        Mockito.doNothing().when(repository).writeTransferTransaction(data);
+
+        SuccessResponse res = service.transfer(data);
+        Assertions.assertFalse(res.operationId().isEmpty());
+    }
+
+    @Test
+    public void confirmShouldReturnSuccessResponse() {
+        String operationId = "123";
+        String code = "1000";
+        ConfirmData data = new ConfirmData(operationId, code);
+        MoneyTransferRepository repository = Mockito.mock(MoneyTransferRepository.class);
+        Mockito.when(repository.getCodeByOperationId(data.getOperationId())).thenReturn(Optional.of(code));
+
+        SuccessResponse res = service.confirm(data);
+        Assertions.assertFalse(res.operationId().isEmpty());
+        Assertions.assertNotEquals(res.operationId(), operationId);
+    }
 }

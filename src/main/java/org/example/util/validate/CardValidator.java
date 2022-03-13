bc1014b18bf0f7c413f8b493172433f337a27fa4
Kenyo-cell <rekurt3000@gmail.com>
2022-03-14 01:53:30 +0300

    feat: created additional card validator class

---
+++ src/main/java/org/example/util/validate/CardValidator.java
@@ -0,0 +1,37 @@
+package org.example.util.validate;
+
+import org.example.exception.IncorrectInputException;
+import org.example.request.transfer.TransferData;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class CardValidator {
+    private final String formatPattern = "MM/yy";
+    private final SimpleDateFormat simpleDateFormat;
+
+    public CardValidator() {
+        simpleDateFormat = new SimpleDateFormat(formatPattern);
+    }
+
+    public void validate(TransferData data) throws IncorrectInputException {
+        if (isCardNumbersAreEqual(data.getCardFromNumber(), data.getCardToNumber()))
+            throw new IncorrectInputException("Card From And Card To Numbers must be different");
+
+        if (!isDateCorrect(data.getCardFromValidTill()))
+            throw new IncorrectInputException("Card has incorrect date");
+    }
+
+    private boolean isCardNumbersAreEqual(String cardFrom, String cardTo) {
+        return cardFrom.equals(cardTo);
+    }
+
+    private boolean isDateCorrect(String date) {
+        try {
+            return simpleDateFormat.parse(date).after(new Date());
+        } catch (ParseException e) {
+            return false;
+        }
+    }
+}

9f591d3e250afc1f94c10aefc98436a04c991c98
Kenyo-cell <rekurt3000@gmail.com>
2022-03-18 01:14:44 +0300

    fix: updated write data record with adding currency field

--- src/main/java/org/example/entity/writer/WriteData.java
+++ src/main/java/org/example/entity/writer/WriteData.java
@@ -2,14 +2,16 @@ package org.example.entity.writer;
 
 import org.example.entity.request.transfer.TransferData;
 
-public record WriteData(String cardFromNumber, String cardToNumber, String date, Integer amount, double percent) {
+public record WriteData(String cardFromNumber, String cardToNumber, String date, Integer amount,
+                        double percent, String currency) {
     public static WriteData from(TransferData data) {
         return new WriteData(
                 data.getCardFromNumber(),
                 data.getCardToNumber(),
                 data.getCardFromValidTill(),
                 data.getAmount().getValue(),
-                data.getAmount().getValue() * 0.1f
+                data.getAmount().getValue() * 0.1f,
+                data.getAmount().getCurrency()
         );
     }
 }
